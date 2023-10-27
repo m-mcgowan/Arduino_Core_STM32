543b8ca854fb87a12632cc9f5e574071c4d2882a
Matthew McGowan <mat.mcgowan+git@gmail.com>
2023-10-26 17:43:13 -0700

    fix(Swan_r5): changes for Windows compatibility

--- system/extras/openocdUpload.sh
+++ system/extras/openocdUpload.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 OPENOCD_PATH="$1"
-OPENOCD_EXTRA_SCRIPTS="$2"
 
-if [ ! -f "${OPENOCD_EXTRA_SCRIPTS}/openocd.cfg" ]; then
-    echo "Could not find ${OPENOCD_EXTRA_SCRIPTS}/openocd.cfg."
+# replace slashes so the shell doesn't think they are excaped characters
+BUILD_PATH="${2//\\//}"
+
+
+if [ ! -f "${BUILD_PATH}/openocd.cfg" ]; then
+    echo "Could not find ${BUILD_PATH}/openocd.cfg."
     exit 1
 fi
 
 # invoke openocd, using the default scripts directory from the openocd path, plus the additional scripts directory given,
 # and finally all the other arguments passed to this script.
-"${OPENOCD_PATH}/bin/openocd" -s "${OPENOCD_PATH}/openocd/scripts" -s "${OPENOCD_EXTRA_SCRIPTS}" -f openocd.cfg "${@:3}"
+"${OPENOCD_PATH}/bin/openocd" -s "${OPENOCD_PATH}/openocd/scripts" -s "${BUILD_PATH}" -f openocd.cfg "${3//\\//}" "${4//\\//}"
